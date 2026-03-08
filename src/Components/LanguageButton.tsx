c258245eff827e9231b6b5c71da8e65791e49ffd
Dominic Billingham <dominicbillingham3232@gmail.com>
2026-03-08 17:54:46 +0000

    Language buttons are now perfected

--- src/Components/LanguageButton.tsx
+++ src/Components/LanguageButton.tsx
@@ -13,25 +13,25 @@ export default function LanguageSwitcher() {
         <div className="fixed bottom-[14vh] right-[calc(50%-min(47vw,59vh)-1vh)] flex flex-col gap-[1vh] z-[10] pointer-events-none">
             <button
                 onClick={() => changeLanguage("en")}
-                className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[2.5vh] py-[1vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${
+                className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[2.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${
                     isEn
                         ? "bg-amber-800 text-white translate-x-[1vh]"
                         : "bg-amber-100/90 text-amber-900 hover:bg-amber-200 translate-x-[0vh]"
                 }`}
                 style={{
-                    clipPath: "polygon(0% 0%, 85% 0%, 100% 50%, 85% 100%, 0% 100%)",
+                    clipPath: "polygon(0% 0%, 100% 0%, 85% 50%, 100% 100%, 0% 100%)",
                 }}
             >English
             </button>
             <button
                 onClick={() => changeLanguage("ca")}
-                className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[2.5vh] py-[1vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${
+                className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[2.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${
                     !isEn
                         ? "bg-amber-800 text-white translate-x-[1vh]"
                         : "bg-amber-100/90 text-amber-900 hover:bg-amber-200 translate-x-[0vh]"
                 }`}
                 style={{
-                    clipPath: "polygon(0% 0%, 85% 0%, 100% 50%, 85% 100%, 0% 100%)",
+                    clipPath: "polygon(0% 0%, 100% 0%, 85% 50%, 100% 100%, 0% 100%)",
                 }}
             >
                 Catalan
