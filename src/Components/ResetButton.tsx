fde6f73dc6095b63f6bc4fd34cea3246c4a1a87e
Dominic Billingham <dominicbillingham3232@gmail.com>
2026-05-10 14:26:35 +0100

    Tidied up missing language sections, fixed button location.

--- src/Components/ResetButton.tsx
+++ src/Components/ResetButton.tsx
@@ -8,7 +8,7 @@ export default function ResetButton() {
     };
 
     return (
-        <div className="fixed top-[2vh] right-[calc(48%-min(47vw,59vh)-1vh)] z-[10] pointer-events-none">
+        <div className="fixed top-[10vh] right-[calc(48%-min(47vw,59vh)-1vh)] z-[10] pointer-events-none">
             <button
                 onClick={handleReset}
                 className="pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[3.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] bg-amber-100/90 text-amber-900 hover:bg-amber-200 translate-x-[0vh]"
