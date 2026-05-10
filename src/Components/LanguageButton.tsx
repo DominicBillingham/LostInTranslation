f34c575f9f7ae383b88de9b2c1875ad424f69320
Dominic Billingham <dominicbillingham3232@gmail.com>
2026-05-10 13:58:21 +0100

    Hidden enlish and catalan buttons for the demo.

--- src/Components/LanguageButton.tsx
+++ src/Components/LanguageButton.tsx
@@ -13,46 +13,32 @@ export default function LanguageSwitcher() {
         <div
             className="fixed bottom-[14vh] right-[calc(48%-min(47vw,59vh)-1vh)] flex flex-col gap-[1vh] z-[10] pointer-events-none">
 
-            <button
-                onClick={() => changeLanguage("en")}
-                className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[3.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${
-                    i18n.language === "en"
-                        ? "bg-amber-800 text-white translate-x-[1vh]"
-                        : "bg-amber-100/90 text-amber-900 hover:bg-amber-200 translate-x-[0vh]"
-                }`}
-                style={{
-                    clipPath: "polygon(0% 0%, 100% 0%, 85% 50%, 100% 100%, 0% 100%)",
-                }}
-            >English
-            </button>
-
-            <button
-                onClick={() => changeLanguage("ca")}
-                className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[3.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${
-                    i18n.language === "ca"
-                        ? "bg-amber-800 text-white translate-x-[1vh]"
-                        : "bg-amber-100/90 text-amber-900 hover:bg-amber-200 translate-x-[0vh]"
-                }`}
-                style={{
-                    clipPath: "polygon(0% 0%, 100% 0%, 85% 50%, 100% 100%, 0% 100%)",
-                }}
-            >
-                Catalan
-            </button>
-
-            <button
-                onClick={() => changeLanguage("fra2")}
-                className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[3.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${
-                    i18n.language === "fra2"
-                        ? "bg-amber-800 text-white translate-x-[1vh]"
-                        : "bg-amber-100/90 text-amber-900 hover:bg-amber-200 translate-x-[0vh]"
-                }`}
-                style={{
-                    clipPath: "polygon(0% 0%, 100% 0%, 85% 50%, 100% 100%, 0% 100%)",
-                }}
-            >
-                French (A2)
-            </button>
+            {/*<button*/}
+            {/*    onClick={() => changeLanguage("en")}*/}
+            {/*    className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[3.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${*/}
+            {/*        i18n.language === "en"*/}
+            {/*            ? "bg-amber-800 text-white translate-x-[1vh]"*/}
+            {/*            : "bg-amber-100/90 text-amber-900 hover:bg-amber-200 translate-x-[0vh]"*/}
+            {/*    }`}*/}
+            {/*    style={{*/}
+            {/*        clipPath: "polygon(0% 0%, 100% 0%, 85% 50%, 100% 100%, 0% 100%)",*/}
+            {/*    }}*/}
+            {/*>English*/}
+            {/*</button>*/}
+            
+            {/*<button*/}
+            {/*    onClick={() => changeLanguage("ca")}*/}
+            {/*    className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[3.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${*/}
+            {/*        i18n.language === "ca"*/}
+            {/*            ? "bg-amber-800 text-white translate-x-[1vh]"*/}
+            {/*            : "bg-amber-100/90 text-amber-900 hover:bg-amber-200 translate-x-[0vh]"*/}
+            {/*    }`}*/}
+            {/*    style={{*/}
+            {/*        clipPath: "polygon(0% 0%, 100% 0%, 85% 50%, 100% 100%, 0% 100%)",*/}
+            {/*    }}*/}
+            {/*>*/}
+            {/*    Catalan*/}
+            {/*</button>*/}
 
             <button
                 onClick={() => changeLanguage("frb1")}
@@ -68,7 +54,20 @@ export default function LanguageSwitcher() {
                 French (B1)
             </button>
 
-
+            <button
+                onClick={() => changeLanguage("fra2")}
+                className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[3.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${
+                    i18n.language === "fra2"
+                        ? "bg-amber-800 text-white translate-x-[1vh]"
+                        : "bg-amber-100/90 text-amber-900 hover:bg-amber-200 translate-x-[0vh]"
+                }`}
+                style={{
+                    clipPath: "polygon(0% 0%, 100% 0%, 85% 50%, 100% 100%, 0% 100%)",
+                }}
+            >
+                French (A2)
+            </button>
+            
         </div>
     );
 }
