16973e6a6c4e055b60550ebdd20e2b554a7aa30c
Dominic Billingham <dominicbillingham3232@gmail.com>
2026-05-18 20:16:28 +0100

    Swapped button order

--- src/Components/LanguageButton.tsx
+++ src/Components/LanguageButton.tsx
@@ -41,20 +41,6 @@ export default function LanguageSwitcher() {
             {/*</button>*/}
 
             <button
-                onClick={() => changeLanguage("frb1")}
-                className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[3.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${
-                    i18n.language === "frb1"
-                        ? "bg-amber-800 text-white translate-x-[1vh]"
-                        : "bg-amber-100/90 text-amber-900 hover:bg-amber-200 translate-x-[0vh]"
-                }`}
-                style={{
-                    clipPath: "polygon(0% 0%, 100% 0%, 85% 50%, 100% 100%, 0% 100%)",
-                }}
-            >
-                Francès (B1)
-            </button>
-
-            <button
                 onClick={() => changeLanguage("fra2")}
                 className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[3.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${
                     i18n.language === "fra2"
@@ -66,6 +52,20 @@ export default function LanguageSwitcher() {
                 }}
             >
                 Francès (A2)
+            </button>
+
+            <button
+                onClick={() => changeLanguage("frb1")}
+                className={`pointer-events-auto transition-all duration-300 pr-[3.5vh] pl-[3.5vh] py-[1.5vh] text-[1.8vh] font-bold uppercase tracking-wider shadow-md hover:translate-x-[0.5vh] ${
+                    i18n.language === "frb1"
+                        ? "bg-amber-800 text-white translate-x-[1vh]"
+                        : "bg-amber-100/90 text-amber-900 hover:bg-amber-200 translate-x-[0vh]"
+                }`}
+                style={{
+                    clipPath: "polygon(0% 0%, 100% 0%, 85% 50%, 100% 100%, 0% 100%)",
+                }}
+            >
+                Francès (B1)
             </button>
             
         </div>
